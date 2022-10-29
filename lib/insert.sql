ee43eace38cd5edbf8644ace3b8faf2716f7f250
luyosamson <luyosamson@gmail.com>
2022-10-29 23:33:11 +0300

    Assignment completed

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive)
+VALUES ("Mr. Chocolate",34,"M","black","high",true);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive)
+VALUES ("Rowdy",20,"F","white","low",true);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive)
+VALUES ("Tabitha",25,"F","white","high",true);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive)
+VALUES ("Sergeant Brown",30,"M","black","low",true);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive)
+VALUES ("Melissa",23,"F","Black","high",true);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive)
+VALUES ("Grinch",25,"M","white","high",true);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive)
+VALUES ("Wendy",23,"F","white","low",false);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive)
+VALUES (NULL,20,"F","white","low",true);
